68ee9ee3caaa4f052969cfc5f75669031fbb1aee
Tom Mullaney <tpmullan@gmail.com>
2016-04-11 15:41:43 +0800

    Allow file name in string

--- lib/carrierwave/base64/base64_string_io.rb
+++ lib/carrierwave/base64/base64_string_io.rb
@@ -12,19 +12,25 @@ module Carrierwave
         raise ArgumentError if encoded_bytes.eql?("(null)")
 
         @file_format = get_file_format description
+        @file_name = get_file_name(description) || "file.#{@file_format}"
         bytes = ::Base64.decode64 encoded_bytes
 
         super bytes
       end
 
       def original_filename
-        File.basename("file.#{@file_format}")
+        File.basename(@file_name)
       end
 
       private
 
       def get_file_format(description)
         regex = /([a-z0-9]+);base64\z/
+        regex.match(description).try(:[], 1)
+      end
+      
+      def get_file_name(description)
+        regex = /data:([\w_\.]+);/
         regex.match(description).try(:[], 1)
       end
     end
